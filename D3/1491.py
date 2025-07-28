4759511917370347bdf152bcd8957fb6b4195d95
Huiwon Moon <tootoo405@gmail.com>
2025-07-28 17:48:53 +0900

    solve 1491 problem

--- D3/1491.py
+++ D3/1491.py
@@ -5,52 +5,6 @@ import math
 def process(N, A, B, R, C):
     return A*abs(R-C) + B*(N-R*C)
 
-
-T = int(input())
-t = 0
-for t in range(T):
-    
-    N, A, B = map(int, input().split())
-    
-    answer = float('inf')
-    for num in range(N, 0, -1):
-        
-        divisors = []
-        for i in range(1, int(math.sqrt(num)) + 1):
-            if num % i == 0:
-                divisors.append(i)
-                if i != num // i:
-                    divisors.append(num // i)
-        
-        divisors.sort()
-
-        if len(divisors) % 2 == 0:
-            R = divisors[len(divisors) // 2]
-            C = divisors[len(divisors) // 2 - 1]
-            value = process(N, A, B, R, C)
-            if value < answer:
-                answer = value
-            
-        
-        else:
-            R = divisors[len(divisors) // 2]
-            C = divisors[len(divisors) // 2]
-            value = process(N, A, B, R, C)
-            if value < answer:
-                answer = value
-        
-                
-        
-    print(f'#{t+1} {answer}')
-
-# %%
-# %%
-import math
-
-
-def process(N, A, B, R, C):
-    return A*abs(R-C) + B*(N-R*C)
-
 T = int(input())
 
 for t in range(T):
