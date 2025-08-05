84f8c81b7d43c6a2e37f6318b391bde2cb3f8390
huiwon <tootoo405@naver.com>
2025-08-05 15:12:35 +0900

    update code

--- SWEA/D3/1208.py
+++ SWEA/D3/1208.py
@@ -12,6 +12,7 @@ for t in range(10):
         min_index = height.index(min_height)
         height[min_index] = min_height + 1
 
+        # 제약조건에 의해
         if max_height - min_height <= 1:
             break
         else:
