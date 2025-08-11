6b91249b1aa364551ed8393fac8a503fc8ec1c7f
huiwon <tootoo405@naver.com>
2025-08-11 16:33:37 +0900

    update code

--- SWEA/D3/1215.py
+++ SWEA/D3/1215.py
@@ -1,35 +1,56 @@
 #%%
 for t in range(10):
+    # 회문의 길이
     length = int(input())
+    # 8*8 글자판
     table = [list(char for char in input()) for _ in range(8)]
+
 
     answer = 0
     for i in range(8):
         for j in range(8):
-            index_list = [i for i in range(length)]
+            # 회문의 길이만큼 index_list 생성
+            # 회문 여부를 판단하는데 사용 (pop(0)과 pop()을 활용하여 양쪽 값을 확인)
+            index_list = [idx for idx in range(length)]
+            
+            # 가로 회문의 개수
             row_cnt = 0
+            # 세로 회문의 개수
             col_cnt = 0
-            # 짝수일때
+            
+            # 회문의 길이가 짝수일때
             if length % 2 == 0:
                 while index_list:
+                    # 시작지점
                     front_idx = index_list.pop(0)
+                    # 끝지점
                     back_idx = index_list.pop()
+                    
+                    # 시작점과 끝지점이 글자판 안에 있고, 회문이면 row_cnt에 더해주기(가로 회문)
                     if 0 <= j+front_idx < 8 \
                         and 0 <= j+back_idx < 8 \
                             and (table[i][j+front_idx] == table[i][j+back_idx]):
                         row_cnt += 1
                     
+                    # 시작점과 끝지점이 글자판 안에 있고, 회문이면 col_cnt에 더해주기(세로 회문)
                     if 0<= i+front_idx < 8 \
                         and 0<= i+back_idx < 8 \
                             and (table[i+front_idx][j] == table[i+back_idx][j]):
                         col_cnt += 1
+                
+                # 양쪽 끝 값이 동일하면 cnt에 더해지는 방식이기 때문에 
+                # row(col)_cnt == length//2 와 같을 때 회문이다.
                 if row_cnt == length // 2:
                     answer += 1
                 if col_cnt == length // 2:
                     answer += 1
-            # 홀수일때
+            
+            # 회문의 길이가 홀수일때
             else:
+                # 가운데 값은 확인할 필요 없으므로 index에서 제거
                 index_list.remove(length // 2)
+                
+                # 양쪽 끝 값을 확인 (length가 짝수일떄와 동일하게 작동)
                 while index_list:
                     front_idx = index_list.pop(0)
                     back_idx = index_list.pop()
@@ -50,19 +71,24 @@ for t in range(10):
     print(f'#{t+1} {answer}')
 
 # %%
-def repeat(row, col, index_list):
-    global row_cnt, col_cnt
-    front_idx = index_list.pop(0)
-    back_idx = index_list.pop()
-    if 0 <= col+front_idx < 8 \
-        and 0 <= col+back_idx < 8 \
-            and (table[row][col+front_idx] == table[row][col+back_idx]):
-        row_cnt += 1
-    
-    if 0<= row+front_idx < 8 \
-        and 0<= row+back_idx < 8 \
-            and (table[row+front_idx][col] == table[row+back_idx][col]):
-        col_cnt += 1
+def check_palindrome(row, col, index_list):
+    global row_cnt, col_cnt, answer
+    while index_list:
+        front_idx = index_list.pop(0)
+        back_idx = index_list.pop()
+        if 0 <= col+front_idx < 8 \
+            and 0 <= col+back_idx < 8 \
+                and (table[row][col+front_idx] == table[row][col+back_idx]):
+            row_cnt += 1
+        
+        if 0<= row+front_idx < 8 \
+            and 0<= row+back_idx < 8 \
+                and (table[row+front_idx][col] == table[row+back_idx][col]):
+            col_cnt += 1
+    if row_cnt == length // 2:
+        answer += 1
+    if col_cnt == length // 2:
+        answer += 1
 
 for t in range(10):
     length = int(input())
@@ -76,20 +102,10 @@ for t in range(10):
             col_cnt = 0
             # 짝수일때
             if length % 2 == 0:
-                while index_list:
-                    repeat(i, j, index_list)
-                if row_cnt == length // 2:
-                    answer += 1
-                if col_cnt == length // 2:
-                    answer += 1
+                check_palindrome(i, j, index_list)
             # 홀수일때
             else:
                 index_list.remove(length // 2)
-                while index_list:
-                    repeat(i, j, index_list)
-                if row_cnt == length // 2:
-                    answer += 1
-                if col_cnt == length // 2:
-                    answer += 1
-
+                check_palindrome(i, j, index_list)
+                
     print(f'#{t+1} {answer}')
--- SWEA/D3/1216.py
+++ SWEA/D3/1216.py
@@ -1,56 +1,70 @@
-
-#%%
-def repeat(row, col, index_list):
+def check_palindrome(row, col, index_list):
     global row_cnt, col_cnt
-    front_idx = index_list.pop(0)
-    back_idx = index_list.pop()
-    if 0 <= col+front_idx < 100 \
-        and 0 <= col+back_idx < 100 \
-            and (table[row][col+front_idx] == table[row][col+back_idx]):
-        row_cnt += 1
+    answer = 0
+    while index_list:
+        front_idx = index_list.pop(0)
+        back_idx = index_list.pop()
+        if 0 <= col+front_idx < 100 \
+            and 0 <= col+back_idx < 100 \
+                and (table[row][col+front_idx] == table[row][col+back_idx]):
+            row_cnt += 1
+        
+        if 0<= row+front_idx < 100 \
+            and 0<= row+back_idx < 100 \
+                and (table[row+front_idx][col] == table[row+back_idx][col]):
+            col_cnt += 1
     
-    if 0<= row+front_idx < 100 \
-        and 0<= row+back_idx < 100 \
-            and (table[row+front_idx][col] == table[row+back_idx][col]):
-        col_cnt += 1
-
+    if row_cnt == length // 2:
+        answer += 1
+    if col_cnt == length // 2:
+        answer += 1
+    
+    return answer
 
 for _ in range(10):
+    # 테스트 케이스 숫자 받기
     t = int(input())
+
+    # 100*100 글자판 생성
     table = [list(char for char in input()) for _ in range(100)]
-    answer = 0
+    
+    # for문에서 벗어나기 위한 설정(break 기능)
     flag = False
+
+    # 가장 긴 회문을 찾아야하기 때문에 100부터 순차적으로 줄어들게 설정
     for length in range(100,0,-1):
         for i in range(100):
             for j in range(100):
+                # index_list를 활용해서 양쪽 값이 같은지 확인
                 index_list = [i for i in range(length)]
+                
+                # 가로 회문 확인하는 변수
                 row_cnt = 0
+                # 세로 회문 확인하는 변수
                 col_cnt = 0
-                # 짝수일때
+                
+                # 회문의 길이가 짝수일때
                 if length % 2 == 0:
-                    while index_list:
-                        repeat(i, j, index_list)
-                    if row_cnt == length // 2:
-                        answer += 1
-                    if col_cnt == length // 2:
-                        answer += 1
+                    # 양쪽 끝 값 전부 확인하는 코드
+                    answer = check_palindrome(i, j, index_list)
                 # 홀수일때
                 else:
+                    # 회문을 판단할 때 필요없는 가운데 index값 제거
                     index_list.remove(length // 2)
-                    while index_list:
-                        repeat(i, j, index_list)
-                    if row_cnt == length // 2:
-                        answer += 1
-                    if col_cnt == length // 2:
-                        answer += 1
+                    
+                    # 양쪽 끝 값 전부 확인하는 코드
+                    answer = check_palindrome(i, j, index_list)
                 
-                if answer >= 1:
+                # answer > 0 이면 회문을 찾은 것
+                if answer > 0:
                     flag = True
                     break
-                
+            
+            # for문 안돌아가게 설정
             if flag:
                 break
         
+        # for문 안돌아가게 설정
         if flag:
             break
 
