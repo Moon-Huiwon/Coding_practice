124f9b7425adddc7bd0827020a9568ebd840b401
huiwon <tootoo405@naver.com>
2025-08-14 16:12:09 +0900

    add description

--- SWEA/D2/1946.py
+++ SWEA/D2/1946.py
@@ -3,12 +3,20 @@ T = int(input())
 
 for t in range(T):
     N = int(input())
+    # 저장하는 장소
     arr = []
+    # 테스트 번호 출력
     print(f'#{t+1}')
+
+    # N번 데이터가 주어진
     for i in range(N):
+        # 문자와 개수 정보 받기
         string, cnt = map(str, input().split())
         cnt = int(cnt)
 
+        # arr이 10개가 될때 까지 저장하고
+        # 10개 넘어가면 print 하고
+        # 다시 초기화해서 저장하도록 설정
         for _ in range(cnt):
             if len(arr) < 10:
                 arr.append(string)
@@ -16,6 +24,8 @@ for t in range(T):
                 print(''.join(map(str, arr)))
                 arr = []
                 arr.append(string)
+        # 마지막에 돌아가는 건 그냥 그대로 출력
+        # 마지막은 10개 미만이어도 출력할 수 있게
         if i == N-1:
             print(''.join(map(str, arr)))
 
