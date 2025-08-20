2a595ba61304a07bec47b6f601dd15c4b29580ad
huiwon <tootoo405@gmail.com>
2025-08-20 11:56:26 +0900

    add code

--- SWEA/D3/1225.py
+++ SWEA/D3/1225.py
@@ -1,6 +1,5 @@
 #%%
-import copy
-
+# 슬라이싱 활용 코드
 for _ in range(10):
 
     t = int(input())
@@ -21,7 +20,24 @@ for _ in range(10):
                 password_arr = password_arr[1:] + [0]
 
     print(f'#{t} {" ".join(map(str, password_arr))}')
-    
-    
 
 # %%
+# pop() 활용 코드 -> 조금 더 빠름
+for _ in range(10):
+
+    t = int(input())
+    password_arr = list(map(int, input().split()))
+
+    flag = True
+    while flag:
+        for i in range(1, 6):
+
+            password = password_arr.pop(0) - i
+            if password > 0:
+                password_arr.append(password)
+            else:
+                password_arr.append(0)
+                flag = False
+                break
+
+    print(f'#{t} {" ".join(map(str, password_arr))}')
