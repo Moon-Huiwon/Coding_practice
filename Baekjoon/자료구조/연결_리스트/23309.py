e41f727252991f7999b50108d0dafa84a1b1e833
huiwon <tootoo405@gmail.com>
2025-09-02 13:40:07 +0900

    gold_4

--- Baekjoon/자료구조/연결_리스트/23309.py
+++ Baekjoon/자료구조/연결_리스트/23309.py
@@ -1,24 +1,20 @@
-# 시간초과 (풀이 중,,,,,)
+# pypy로 진행할 때 돌아감
+# python으로 진행하면 시간초과,,,
 import sys
 input = sys.stdin.readline
-
-from collections import deque
 
 N, M = map(int, input().split())
 station = list(map(int, input().split()))
 
-# 여기서 시간초과 발생 / 전체를 쓰려고 했는데,, 불필요한 리스트 개수라 시간초과,,,
 max_num = 1000001
-pre_num = deque([[] for _ in range(max_num)])
-post_num = deque([[] for _ in range(max_num)])
+pre_num = [0] * max_num
+post_num = [0] * max_num
 
 for i in range(N):
-    pre_num[station[i]].append(station[i-1])
-    
-    if i == N-1:
-        post_num[station[i]].append(station[0])    
-    else:
-        post_num[station[i]].append(station[i+1])
+    pre_num[station[i]] = station[i-1]
+    post_num[station[i]] = station[i+1] if i < N-1 else station[0]
+
+result = []
 
 for _ in range(M):
     cmd_list = list(map(str, input().split()))
@@ -29,31 +25,93 @@ for _ in range(M):
         j = int(cmd_list[2])
     
     if cmd == 'BN':
-        num = post_num[i].pop()
-        print(num)
-        if len(post_num[j]) == 0:
+        station_num = post_num[i]
 
-            post_num[i].append(j)
-            post_num[j].append(num)
+        post_num[i] = j
+        post_num[j] = station_num
 
-            pre_num[num].pop()
-            pre_num[num].append(j)
-            pre_num[j].append(i)
-        
-    elif cmd == 'BP':
-        num = pre_num[i].pop()
-        print(num)
-        if len(pre_num[j]) == 0:
-            pre_num[i].append(j)
-            pre_num[j].append(num)
-
-            post_num[num].pop()
-            post_num[num].append(j)
-            post_num[j].append(i)
+        pre_num[station_num] = j
+        pre_num[j] = i
     
+    elif cmd == 'BP':
+        station_num = pre_num[i]
+
+        pre_num[i] = j
+        pre_num[j] = station_num
+
+        post_num[station_num] = j
+        post_num[j] = i
+
     elif cmd == 'CN':
-        print(post_num[i].pop())
+        station_num = post_num[i] # 폐쇄할 station
+        post_num[i] = post_num[station_num] # 폐쇄한 station의 다음 역
+        pre_num[post_num[station_num]] = i
     
     else:
-        print(pre_num[i].pop())
+        station_num = pre_num[i]
+        pre_num[i] = pre_num[station_num]
+        post_num[pre_num[station_num]] = i
 
+    result.append(str(station_num))
+
+print('\n'.join(result))
+
+#%%
+# 시간초과
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+station = list(map(int, input().split()))
+
+pre_num = {} # 이전 역 저장
+post_num = {} # 다음 역 저장
+
+for i in range(N):
+    pre_num[station[i]] = station[i-1]
+    post_num[station[i]] = station[i+1] if i < N-1 else station[0]
+
+result = []
+
+for _ in range(M):
+    cmd_list = list(map(str, input().split()))
+    cmd = cmd_list[0]
+    i = int(cmd_list[1])
+    if len(cmd_list) > 2:
+        j = int(cmd_list[2])
+    
+    if cmd == 'BN':
+        station_num = post_num[i]
+
+        post_num[i] = j
+        post_num[j] = station_num
+
+        pre_num[station_num] = j
+        pre_num[j] = i
+    
+    elif cmd == 'BP':
+        station_num = pre_num[i]
+
+        pre_num[i] = j
+        pre_num[j] = station_num
+
+        post_num[station_num] = j
+        post_num[j] = i
+    
+    elif cmd == 'CN':
+        station_num = post_num[i] # 폐쇄할 station
+        post_num[i] = post_num[station_num] # 폐쇄한 station의 다음 역
+        pre_num[post_num[station_num]] = i
+
+        del post_num[station_num], pre_num[station_num] # 시간초과 발생
+    
+    else:
+        station_num = pre_num[i]
+        pre_num[i] = pre_num[station_num]
+        post_num[pre_num[station_num]] = i
+
+        del post_num[station_num], pre_num[station_num] # 시간초과 발생
+    
+    result.append(str(station_num))
+
+print('\n'.join(result))
