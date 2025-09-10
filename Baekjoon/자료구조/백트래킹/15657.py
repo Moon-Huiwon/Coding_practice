653f5d0dac88e24a39ee5dc1f210722e7f7a0684
huiwon <tootoo405@gmail.com>
2025-09-11 08:27:09 +0900

    silver_3

---
+++ Baekjoon/자료구조/백트래킹/15657.py
@@ -0,0 +1,16 @@
+def backtracking(start, lst):
+    if len(lst) == M:
+        result.append(lst)
+        return
+    
+    for i in range(start, N):
+        backtracking(i, lst+[nums[i]])
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+result = []
+backtracking(0,[])
+
+for i in range(len(result)):
+    print(*result[i])
